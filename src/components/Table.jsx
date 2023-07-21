e9872438978d9f49c97e3e546e6706b242147ddf
parunkov <parunkov@gmail.com>
2023-07-21 16:55:25 +0500

    table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,8 +6,29 @@ const Table = ({posts, currentPage, loadPosts}) => {
         console.log(posts);
     }, []);
 
+    const TableRow = ({id, title, body}) => {
+        return (
+            <tr>
+                <td>{id}</td>
+                <td>{title}</td>
+                <td>{body}</td>
+            </tr>
+        )
+    }
+
     return (
-        <div>AAA</div>
+        <div>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>ID</th>
+                        <th>Заголовок</th>
+                        <th>Описание</th>
+                    </tr>
+                    {posts.map((post) => <TableRow key={post.id} id={post.id} title={post.title} body={post.body} />)}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
--- src/redux/posts-reducer.js
+++ src/redux/posts-reducer.js
@@ -26,7 +26,6 @@ export const load = (posts) => ({
 export const loadPosts = (currentPage) => async(dispatch) => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_start=${currentPage * 10 - 1}&_limit=10`);
     const posts = await response.json();
-    console.log("🚀 ~ file: posts-reducer.js:30 ~ loadPosts ~ posts:", posts);
     dispatch(load(posts));
 }
 
