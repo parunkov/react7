687f4fee443feebfb2da4220d3b6c4b1fa183f3a
parunkov <parunkov@gmail.com>
2023-07-26 08:49:00 +0500

    table title

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from 'react';
+import styles from './Table.module.scss';
+import cn from 'classnames';
+console.log(styles);
 
 const Table = ({ posts, currentPage, loadPosts }) => {
     const [filteredPocts, setFilteredPosts] = useState(posts);
@@ -6,7 +9,7 @@ const Table = ({ posts, currentPage, loadPosts }) => {
         value: '',
         column: null,
         numberSort: true,
-        reverseSort: false 
+        reverseSort: false
     });
     const [savedPage, setSavedPage] = useState(currentPage);
 
@@ -19,7 +22,7 @@ const Table = ({ posts, currentPage, loadPosts }) => {
                 value: '',
                 column: null,
                 numberSort: true,
-                reverseSort: false 
+                reverseSort: false
             });
             loadPosts(currentPage);
         }
@@ -28,8 +31,8 @@ const Table = ({ posts, currentPage, loadPosts }) => {
     const filterSortPosts = (posts, { value, column, numberSort, reverseSort }) => {
         let newPosts = [];
         const newFilterdPosts = posts.filter((item) => item.id.toString().includes(value)
-        || item.title.toLowerCase().includes(value.toLowerCase())
-        || item.body.toLowerCase().includes(value.toLowerCase()));
+            || item.title.toLowerCase().includes(value.toLowerCase())
+            || item.body.toLowerCase().includes(value.toLowerCase()));
         if (!column) {
             newPosts = newFilterdPosts;
         } else {
@@ -44,22 +47,37 @@ const Table = ({ posts, currentPage, loadPosts }) => {
     };
 
     const onFilterInput = (event) => {
-        const newFilterValue = {...filterValue, value: event.target.value};
+        const newFilterValue = { ...filterValue, value: event.target.value };
         setFilterValue(newFilterValue);
         setFilteredPosts(filterSortPosts(posts, newFilterValue));
     }
 
     const onColumnClick = (event) => {
         if (event.target.dataset.name === filterValue.column) {
-            const newFilterValue = {...filterValue, reverseSort: !filterValue.reverseSort};
+            const newFilterValue = { ...filterValue, reverseSort: !filterValue.reverseSort };
             setFilterValue(newFilterValue);
             setFilteredPosts(filterSortPosts(posts, newFilterValue));
         } else {
-            const newFilterValue = {...filterValue, column: event.target.dataset.name, numberSort: event.target.dataset.name === 'id', reverseSort: false};
+            const newFilterValue = { ...filterValue, column: event.target.dataset.name, numberSort: event.target.dataset.name === 'id', reverseSort: false };
             setFilterValue(newFilterValue);
             setFilteredPosts(filterSortPosts(posts, newFilterValue));
         }
     }
+
+    const TableTitle = ({ id, column, reverseSort, onClick, text }) => {
+        console.log(reverseSort ? cn(styles.sort, styles.reverse) : styles.sort);
+        return (
+            <th className={styles.title} data-name={id} onClick={onClick}>
+                {text}
+                {column === id ? <span className={reverseSort ? cn(styles.sort, styles.reverse) : styles.sort}>
+                    <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <line x1="0.353553" y1="0.646447" x2="6.18011" y2="6.47301" stroke="#FCFCFC" />
+                        <line x1="5.64645" y1="6.30331" x2="11.3033" y2="0.646453" stroke="white" />
+                    </svg>
+                </span> : ''}
+            </th>
+        )
+    };
 
     const TableRow = ({ id, title, body }) => {
         return (
@@ -77,9 +95,27 @@ const Table = ({ posts, currentPage, loadPosts }) => {
             <table>
                 <tbody>
                     <tr>
-                        <th data-name="id" onClick={onColumnClick}>ID</th>
-                        <th data-name="title" onClick={onColumnClick}>Заголовок</th>
-                        <th data-name="body" onClick={onColumnClick}>Описание</th>
+                        <TableTitle
+                            id={'id'}
+                            column={filterValue.column}
+                            reverseSort={filterValue.reverseSort}
+                            onClick={onColumnClick}
+                            text={'ID'}
+                        />
+                        <TableTitle
+                            id={'title'}
+                            column={filterValue.column}
+                            reverseSort={filterValue.reverseSort}
+                            onClick={onColumnClick}
+                            text={'Заголовок'}
+                        />
+                        <TableTitle
+                            id={'body'}
+                            column={filterValue.column}
+                            reverseSort={filterValue.reverseSort}
+                            onClick={onColumnClick}
+                            text={'Описание'}
+                        />
                     </tr>
                     {filteredPocts.map((post) => <TableRow key={post.id} id={post.id} title={post.title} body={post.body} />)}
                 </tbody>
